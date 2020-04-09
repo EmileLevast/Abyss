ec4f83c73ce92565d19ec72a3a90137fbb2c3e50
EmileLevast <emilelevast@outlook.fr>
2020-04-09 12:01:22 +0200

    images ally implemented, images lord added to drawable

--- app/src/main/java/fr/emile/abyss/modelClass/gameItems/FishType.kt
+++ app/src/main/java/fr/emile/abyss/modelClass/gameItems/FishType.kt
@@ -3,9 +3,9 @@ package fr.emile.abyss.modelClass.gameItems
 import fr.emile.abyss.R
 
 enum class FishType(val imgId:Int) {
-    CRAB(R.drawable.jellyfish_ally),
+    CRAB(R.drawable.crab_ally),
     JELLYFISH(R.drawable.jellyfish_ally),
-    OCTOPUS(R.drawable.jellyfish_ally),
-    SEA_HORSE(R.drawable.jellyfish_ally),
-    SEA_SHELL(R.drawable.jellyfish_ally)
+    OCTOPUS(R.drawable.octopussy_ally),
+    SEA_HORSE(R.drawable.sea_horse_ally),
+    SEA_SHELL(R.drawable.sea_shell_ally)
 }
