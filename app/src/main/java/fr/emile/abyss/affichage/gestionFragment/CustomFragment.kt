8b44f57dc91021ecdef12039b61ba50ad7ab3a64
EmileLevast <emilelevast@outlook.fr>
2020-04-12 14:22:01 +0200

    add background to custom fragment

--- app/src/main/java/fr/emile/abyss/affichage/gestionFragment/CustomFragment.kt
+++ app/src/main/java/fr/emile/abyss/affichage/gestionFragment/CustomFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import fr.emile.abyss.R
 
 /**
  * the type parameter given to [CustomFragment] is the type needed for the parameter in [updateView] function
@@ -15,6 +16,11 @@ abstract class  CustomFragment<T> : Fragment(){
     when you override a customFragment give it a [idLayoutToInflate]
      */
     abstract val idLayoutToInflate:Int
+    //represent the id of root view that contains the other view in the layout
+    //I use it to prevent click on the screen behind the frag
+    open val idRootView:Int=R.id.rootView
+    //I give a default value to the root id, so you just has to give this id to the root view
+    // in your layout file
 
     /**called in onCreateView()
      *instantiate some view here if you have to do so (typeface, programmatically UI)
@@ -28,12 +34,25 @@ abstract class  CustomFragment<T> : Fragment(){
     //just get the layout to inflate, create the view
     final override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val viewInflated = inflater.inflate(idLayoutToInflate, container, false)
+
+        /*prepare the background of the fragment*/
+        val rootView:View=viewInflated.findViewById(idRootView)
+        //we prevent more click on the screen behind
+        rootView.setOnTouchListener{_,_->true}
+        //we set a background to see a fragis launched
+        rootView.setBackgroundResource(R.color.backgroundFragment)
+
+
         createView(viewInflated)
         return viewInflated
     }
 
     //nothing important
     final override fun onActivityCreated(savedInstanceState: Bundle?) {
+
+
+
+
         super.onActivityCreated(savedInstanceState)
     }
 
